cefef517651d07b1fe5502f06054690192172d58
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-06-13 11:45:33 -0500

    created more books and added local-storage

--- books.js
+++ books.js
@@ -49,13 +49,19 @@ const bookCreator = (title, author, genre, isbnNumb) => {
 
 const elonMusk = bookCreator("Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future", "Ashlee Vance", "Biography", "978-0062301239");
 
-const mobyDick = bookCreator("Moby-Dick", "Herman Melville", "Epic", "978-0-321-22800-0");
+const steveJobs = bookCreator("Steve Jobs", "Walter Isaacson", "Biography", "1-4516-4853-7");
+
+const mobyDick = bookCreator("Moby-Dick", "Herman Melville", "Fiction", "978-0-321-22800-0");
 
 const scarTissue = bookCreator("Scar Tissue", "Anthony Kiedis", "Autobiography", "1-4013-0101-0");
 
+const gandhi = bookCreator("The Story of My Experiments with Truth", "Mahatma Gandhi", "Autobiography", "978-0486245935")
+
 const life3 = bookCreator("Life 3.0", "Max Tegmark", "Sci-Fi", "978-1-101-94659-6");
 
-const mockingbird = bookCreator("To Kill A Mocking Bird", "Harper Lee", "Southern Gothic", "978-0446310789");
+const martian = bookCreator("The Martian", "Andy Weir", "Sci-Fi", "978-0-8041-3902-1");
+
+const mockingbird = bookCreator("To Kill A Mocking Bird", "Harper Lee", "Fiction", "978-0446310789");
 
 const gatsby = bookCreator("The Great Gatsby", "F. Scott Fitzgerald", "Fiction", "978-0743273565");
 

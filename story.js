a71fddc80c4a7952fb6e7d125214917417a9f190
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-06-13 14:20:53 -0500

    added a side bar to DOM

--- story.js
+++ story.js
@@ -1,3 +1,31 @@
+const sideBar = document.querySelector("#side-bar");
+const fragment = document.createDocumentFragment();
+
+const booksAvailable = () => {
+        let booksHeader = document.createElement("h3");
+        booksHeader.innerHTML = `Library Inventory:`;
+        let booksDesc = document.createElement("h5");
+        booksDesc.innerHTML = `Green = Available | Red = Unavailable`;
+        fragment.appendChild(booksHeader);
+        fragment.appendChild(booksDesc);
+    for (let avail in library) {
+        if (library[avail].checkedOut === false) {
+            let available = document.createElement("li");
+            available.innerHTML = `${avail}`;
+            available.style.color = "green";
+            fragment.appendChild(available);
+        } else if (library[prop].checkedOut === true) {
+            let unavailable = document.createElement("li");
+            unavailable.innerHTML = `${prop}`;
+            unavailable.style.color = "red";
+            fragment.appendChild(unavailable);
+        } 
+    }
+    sideBar.appendChild(fragment);
+}
+
+booksAvailable();
+
 const subButton = document.querySelector('#checkout')
 const retButton = document.querySelector('#return')
 
